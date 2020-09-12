4b0670c47fb225d045ef8385b277e82140374ca0
Kyle Rohr <kyle@antipex.com>
2020-09-11 20:35:14 -0700

    Initial Commit

---
+++ Lighthouse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lighthouse
+//
+//  Created by Kyle Rohr on 11/9/20.
+//  Copyright © 2020 Kyle Rohr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
